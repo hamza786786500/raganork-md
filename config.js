311821253fcfb7f7b2b1c745333a73235e33f951
hamza786786500 <anasala120o@gmail.com>
2025-01-27 12:36:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~7dc29ae2e9b874acb917fb9be34a0908:6c726170684d4c4c6e66303d').trim() || '',
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~b38b47f18b285eabd7d899de46155a72:684166624d5757513863633d').trim() || '',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || 'Raganork;Skl11;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
